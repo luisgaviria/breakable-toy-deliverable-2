f0db03a89f5e509bf49e6e95d2aed965e87fcc14
Luis Gaviria <luisgaviria@MacBook-Pro-2.local>
2021-02-26 12:27:40 -0500

    styling commit

--- server/src/routes/api/v1/storiesRouter.js
+++ server/src/routes/api/v1/storiesRouter.js
@@ -12,7 +12,7 @@ storiesRouter.use("/:storyId/reviews", storyReviewsRouter);
 
 storiesRouter.get("/", async (req, res) => {
   try {
-    const stories = await Story.query().orderBy("publishedAt", "desc");
+    const stories = await Story.query().orderBy("publishedAt", "desc").limit("20");
     const serializedStories = [];
     for (const story of stories) {
       const serializedStory = await StorySerializer.showData(story);
