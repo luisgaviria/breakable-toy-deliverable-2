c7da7818391f9202ee06fb0e4d1632c1bcdbf92a
Luis Gaviria <luisgaviria@MacBook-Pro-2.local>
2021-02-27 10:35:14 -0500

    Styling and twitter api attempt

---
+++ server/src/apiClient/twitterApiClient.js
@@ -0,0 +1,13 @@
+// import Twitter from "twitter";
+// require("dotenv/config");
+
+// const apiKey = process.env.apiKey;
+// const apiSecretKey = process.env.apikeySecret;
+// const accessToken = process.env.accessTokensecret;
+
+// const client = new Twitter({
+//   consumer_key: apiKey,
+//   consumer_secret: apiSecretKey,
+//   access_token_key: accessToken,
+//   access_token_secret: accessTokenSecret,
+// });
