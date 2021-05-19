bf34df3ced9c4b8be4219529dd6c9afbda6450cc
Luis Gaviria <luisgaviria@MacBook-Pro-2.local>
2021-05-19 17:27:28 -0400

    new style edits

---
+++ client/src/components/layout/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./style.css";
+
+const Header = () => {
+  return (
+    <div className="not-container">
+      <h1>Making it right.</h1>
+      <h2>
+        We design, develop, and deliver experiences that are technically
+        impressive and easy to use.
+      </h2>
+    </div>
+  );
+};
+
+export default Header;
